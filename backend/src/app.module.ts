6c06d02a7f13e306bcd5f5b4e18460b005873f69
8-1000-e <emiledubdart@gmail.com>
2026-08-19 18:49:30 +0200

    format backend src with tabs (editorconfig)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [],
-  providers: [],
+	imports: [PrismaModule],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
